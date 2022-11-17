7606daa23c826d3e907b1a5f87046bf49a430c26
Nemolite <g25092011@mail.ru>
2022-11-17 10:58:24 +0300

    index

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* Маршрут для главной страницы*/
+Route::get('/',[\App\Http\Controllers\MainController::class, 'index'])->name('index');
